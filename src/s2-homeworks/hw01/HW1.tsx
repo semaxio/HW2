40c5eb1fc5f577895d33c5b3e591b39230d39175
semaxio <kutchms@gmail.com>
2025-01-29 17:56:26 +0300

    redeploy

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -59,13 +59,10 @@ const HW1 = () => {
       <div id={'hw1'}>
         <div className={s2.hwTitle}>Homework #1</div>
         <div className={s2.hw}>
-          {/*проверка отображения (не менять)*/}
           <div className={s2.chat}>
             <Message message={message0}/>
             <FriendMessage message={friendMessage0}/>
           </div>
-
-          {/*для автоматической проверки дз (не менять)*/}
           <MessageSender M={Message}/>
         </div>
       </div>
